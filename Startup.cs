3d395548c42113b8d2f2160b402e7e37afb37774
LMichael <lmichael1003@gmail.com>
2019-10-29 19:05:57 +0200

    fix

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace BirthdayBot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<AppDbContext>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
